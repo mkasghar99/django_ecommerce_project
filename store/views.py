ca6a1221a2d37ba4e28b0df3aa444cb062c4cd95
Malik <mkasghar99@gmail.com>
2023-09-18 19:42:09 +0500

    crud completed

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 # store/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
+from .forms import ProductForm
 
 def product_list(request):
     products = Product.objects.all()
@@ -9,3 +10,31 @@ def product_list(request):
 def product_detail(request, pk):
     product = Product.objects.get(pk=pk)
     return render(request, 'store/product_detail.html', {'product': product})
+
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'store/product_form.html', {'form': form})
+
+def product_update(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'store/product_form.html', {'form': form})
+
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'store/product_confirm_delete.html', {'product': product})
